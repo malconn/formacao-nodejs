f1eae7d13ada9e3ab3bbd012ea751f648e9c7042
malconn <malconaugusto@outlook.com>
2022-08-18 23:27:18 -0300

    feat: rotas no Express

--- Express/index.js
+++ Express/index.js
@@ -2,6 +2,19 @@ const express = require('express'); // Importando o Express
 const app = express(); // Iniciando o express
 
 
+app.get("/",function(req,res){
+ res.send("Bem vindo");
+ // res.send("Outro dado");// Não funciona mais de uma vez, pois é fechada a conexão com o cliente
+});
+
+app.get("/blog",function(req,res){
+ res.send('Bem vindo ao meu blog');
+});
+
+app.get("/canal/youtube",function(req,res){
+ res.send('Bem vindo ao meu canal!');
+});
+
 app.listen(4000,function(erro){
  if(erro){
   console.log('Um erro ocorreu!');
